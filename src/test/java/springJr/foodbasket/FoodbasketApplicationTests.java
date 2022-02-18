2fd42357091df640b9b9e7cba425bb909f579450
TaeyoungNoah <tyty09171007@gmail.com>
2022-02-18 18:34:20 +0900

    feat: 프로젝트 초기 설정

---
+++ src/test/java/springJr/foodbasket/FoodbasketApplicationTests.java
@@ -0,0 +1,13 @@
+package springJr.foodbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodbasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
